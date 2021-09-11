f889dd55a62480257f7174d3bd8e415ec6cdfb3b
parth1208 <56910459+parth1208@users.noreply.github.com>
2021-09-12 00:50:07 +0530

    conserch

--- Test/urls.py
+++ Test/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('login/',views.login,name='login'),
     path('detail/<int:pk>',views.detail,name='detail'),
+    path('serch_result/',view=views.search,name='search'),
+    path('Content/',view=views.content,name='content'),
+    path('most/',view=views.most,name='most'),
 ]
